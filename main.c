c7456dc6759639a7f6e8ecbc5dfe3363de5d0c83
Fredmaster08 <frxdmaster@gmail.com>
2026-04-09 14:00:09 +0200

    colors

--- main.c
+++ main.c
@@ -9,6 +9,12 @@
 #define WORDLE 5
 #define MAX_SIZE 256
 
+#define YELLOW "\x1b[33m"
+#define GREEN "\x1b[32m"
+
+#define CLEAR "\x1B[2J\x1B[H"
+#define BOLD "\x1b[1m"
+#define RESET "\x1b[0m"
 
 void init_board(char board[MAX_ROWS][MAX_LETTERS]) {
     for (int i = 0; i < MAX_ROWS; i++) {
@@ -33,40 +39,61 @@ bool validate_input(char buffer[MAX_SIZE]) {
     return true;
 }
 
-void print_board(char board[MAX_ROWS][MAX_LETTERS]) {
+void print_board(char board[MAX_ROWS][MAX_LETTERS], const char* wordle) {
     for (int i = 0; i < MAX_ROWS; i++) {
         for (int j = 0; j < MAX_LETTERS; j++) {
-            printf("%c ", board[i][j]);
+            if(board[i][j] == wordle[j]) {
+                printf(GREEN BOLD"%c " RESET, board[i][j]);
+            }
+            else {
+                bool found = false;
+                for (int k = 0; k < WORDLE; k++) {
+                    if (board[i][j] == wordle[k]) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found) {
+                    printf(YELLOW "%c " RESET, board[i][j]);
+                } else {
+                    printf("%c ", board[i][j]);
+                }
+            }
         }
         printf("\n");
     }
 }
 
-void fill_in(char board[MAX_ROWS][MAX_LETTERS], char buffer[MAX_SIZE]) {
-    for (int i = 0; i < strlen(buffer) - 1; i++) {
-        buffer[i] = board[i];
+void fill_in(char board[MAX_ROWS][MAX_LETTERS], char buffer[MAX_SIZE], int* row) {
+    for (int i = 0; i < WORDLE; i++) {
+        board[*row][i] = toupper(buffer[i]);
     }
+    (*row)++;
 }
 
 int main() {
 
+    printf(CLEAR);
+
     char board[MAX_ROWS][MAX_LETTERS];
+    int row = 0;
+    const char* wordle = "FIRES";
 
     init_board(board);
+    print_board(board, wordle);
 
     while(1) {
 
-        print_board(board);
         char buffer[MAX_SIZE];
         fgets(buffer, MAX_SIZE, stdin);
-        printf("%s\n", buffer);
-        printf("%d\n", strlen(buffer) - 1);
     
-
         if (!validate_input(buffer)) {
             continue;
         }
-        fill_in(board, buffer);
+        fill_in(board, buffer, &row);
+        printf(CLEAR);
+        print_board(board, wordle);
         
     }           
 
